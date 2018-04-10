3d40fc73ce5d816a687d040f35b0284a8d3d4b00
root <root@instance-3.c.endless-duality-157610.internal>
2018-04-10 08:50:15 +0000

    add feature 3

--- src/ead/HelloWorld.java
+++ src/ead/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Feature 1 Added");
         System.out.println("Feature 2 Added");
+	System.out.println("Feature 3")
+
     }
 }
