4626262c4f5c21c41075a337d0c21d5f136c9fe4
root <root@practice.c.endless-duality-157610.internal>
2018-04-29 10:19:24 +0000

    hello

--- src/ead/HelloWorld.java
+++ src/ead/HelloWorld.java
@@ -7,7 +7,8 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Feature 1 Added");
         System.out.println("Feature 2 Added");
-	System.out.println(Feature 3 added to this");
+	System.out.println("Feature 3 added");
+	System.out.print("Hey there!");
 
     }
 }
